1e724b37482a4087d297d62cc83d7c046a082fd8
shahrukhx01 <sk28671@gmail.com>
2023-05-10 07:56:47 +0200

    feat: add chunked cross attention intuition

---
+++ transformer_architectures/gpt/model/gpt.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class GPT(nn.Module):
+    def __init__(self, embedding, decoder, generator):
+        super(GPT, self).__init__()
+        self.embedding = embedding
+        self.decoder = decoder
+        self.generator = generator
+    
+    def forward(self, x, mask):
+        memory = None
+        return self.decoder(self.embedding(x), memory, src_mask=None, tgt_mask=mask)
